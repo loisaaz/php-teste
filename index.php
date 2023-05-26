cc9297efb5e89795731a4a2292a9fd83eb68e086
heloisa <125582181+loisaaz@users.noreply.github.com>
2023-05-26 11:52:51 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 
 <?php
-print "Olaaaa";
+/*print "Olaaaa";
 echo "<br/>";
 // variável string
 $aluno = "Heloisa";
@@ -19,12 +19,12 @@ $altura = 1.68;
 
 echo " Aluno: ".$aluno." Idade: ".$idade;
 echo "<br/>";
-echo " Altura: ".$altura;
+echo " Altura: ".$altura;*/
 /*var_dump($altura);*/
 
 /*EXERCÍCIO UM*/
         // valores digitados
-        $valor1 = 100;
+/*       $valor1 = 100;
         $valor2 = 150;
         $valor3 = 750;
 
@@ -32,10 +32,10 @@ echo " Altura: ".$altura;
         $soma = $valor1 + $valor2 + $valor3;
 		echo "<br/>";
         echo "A soma dos valores é: " . $soma;
-	echo "<br/>";
+	echo "<br/>";*/
 	
 	/*EXERCÍCIO DOIS*/
-    echo "Digite suas notas";
+/*    echo "Digite suas notas";
         $valor1 = 9;
         $valor2 = 6;
         $valor3 = 8;
@@ -45,7 +45,38 @@ $soma = $valor1 + $valor2 + $valor3 /3;
 		echo "<br/>";
         echo "Sua média é: " . $soma;
 		echo "<br/>";
-		echo "Arredondada para 7,6";
+		echo "Arredondada para 7,6";*/
+		
+/*$valor1 = 5;
+$valor2 = 10;		
+(expressão) ? (valor verdadeiro) : (valor falso);
+($valor1 == $valor2) ? $resultado = true : $resultado = false;	
+var_dump($resultado);
+		
+$v1 = true;
+$v2 = false;		
+var_dump((10 == 10) && (20 == 20))*/	
+
+/*$nome = 'Heloisa ';
+$sobrenome = 'Marinho';
+echo $nome.$sobrenome;*/	
+	
+/*echo "Tabuada";	
+echo "<br/>";
+for($i=1;$i<=10;$i++){
+echo $i;
+}*/	
+		
+echo "IMC";
+echo "<br/>";
+$valor1 = 52;
+$valor2 = 1.68;
+$soma = $valor2 * $valor2;
+$resultado = $valor1 / $soma;	
+echo " Seu IMC é:".$resultado;		
+echo(round(18,42403628117954) . "<br>");
+
+		
 ?>
 </body>
 </html>
